7353714e4716e190e85fe5e046d2b2e8676a9314
evrseen <wellyaz.id@gmail.com>
2023-05-19 10:42:41 +0700

    hashing password

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { User } from "./user";
 
-
-const Home = () => {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  
   return (
-   <div> Home Page</div>
-  );
+    <main>
+      <h2>Server Session</h2>
+      <pre>{JSON.stringify(session)}</pre>
+      <h2>Client Call</h2>
+      <User/>
+    </main>
+  )
 };
-
-export default Home;
